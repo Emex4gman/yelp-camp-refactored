cea2ca2cffdd1f9dea2c0b61a7f03e107f31ff9c
CHUKWUEMEKA IBEBUGWU <emex4gman@gmail.com>
2019-06-25 18:09:11 +0100

    Changed the port on app.js
    Changed the port on app.js

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
-});
+var port_number = process.env.PORT || 3000;
+app.listen(port_number);
+
